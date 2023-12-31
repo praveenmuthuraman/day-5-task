722e6a53347f227ebce5cf7181c1bf8a3e703311
praveenmuthuraman <praveenmuthuraman4756@gmail.com>
2023-12-31 10:07:03 +0530

    DAY 5 Task

---
+++ JSON resume/script.js
@@ -0,0 +1,51 @@
+let myResume = {
+  basics: {
+    name: "PRAVEEN M",
+    email: "praveenmuthuramn4756@gmail.com",
+    phone: 8637485983,
+    degree: "B.E",
+    location: {
+      address: "A.PUTHUPATTI ARIYAPATTI PO USILAMPATTI",
+      postalCode: 625532,
+      city: "MADURAI",
+      state: "Tamilnadu",
+      country: "India",
+    },
+    profiles: [
+      {
+        github: "https://github.com/praveenmuthuraman",
+      },
+    ],
+  },
+  work: [
+    {
+      company: "SUTHERLAND",
+      position: "VOICE PROCESS",
+      summary:
+        "I Am collecting Fund For Pre-mature baby to get ventilators",
+    },
+  ],
+  education: [
+    {
+      institution: "FATHIMA MICHEAL COLLEGE OF ENGINEERING",
+      department: "ECE",
+      studyType: "fulltime",
+      "batch start year": 2018,
+      "batch end year": 2022,
+      gpa: 7.5,
+    },
+  ],
+  
+
+  languages: [
+    {
+      language: "Tamil,Enlish",
+    },
+  ],
+  interests: [
+    {
+      name: "script writter,youtuber,",
+    },
+  ],
+};
+console.log(myResume);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # day-5-task
 json
+1. For the given JSON iterate over all for loops (for, for in, for of, forEach)
